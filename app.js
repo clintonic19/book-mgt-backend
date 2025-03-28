fe367ec9a27fb5875fd6126bc6d6464c9fe23f20
CLINTON CLINTON <44939329+clintonic19@users.noreply.github.com>
2025-03-28 02:33:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 //Cross-Origin Resource Sharing (CORS)
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5002', "https://book-mgt-backend.onrender.com", "https://book-mgt.vercel.app"],
+  origin: ['http://localhost:5173', 'http://localhost:5002', "https://book-mgt.vercel.app"],
 //   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization" || "authorization"]
