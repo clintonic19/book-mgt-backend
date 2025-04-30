10e673c1a26c4e38bb56b275620e5296f40af92d
clintonic <clintonictendency@gmail.com>
2025-04-30 09:23:16 +0100

    Updated Cart.jsx

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5002', "https://book-mgt.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization" || "authorization"]
+  allowedHeaders: ["Content-Type", "Authorization" || "authorization", "Accept-Version"] 
 }));
 
 //Connect to database
